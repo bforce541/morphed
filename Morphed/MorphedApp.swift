7a8bfab4064ab519450f24ddde4e9932831991aa
bforce541 <bforce54@gmail.com>
2026-01-03 12:10:37 -0600

    Initial commit

---
+++ Morphed/MorphedApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorphedApp.swift
+//  Morphed
+//
+//  Created by Yoshua on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MorphedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MorphedTests/MorphedTests.swift
@@ -0,0 +1,17 @@
+//
+//  MorphedTests.swift
+//  MorphedTests
+//
+//  Created by Yoshua on 1/3/26.
+//
+
+import Testing
+@testable import Morphed
+
+struct MorphedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
